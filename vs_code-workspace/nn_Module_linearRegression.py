cbc57ff0a1b10f067eab57123b89dd99b6a2b330
kimhyungkyu-1208 <83112028+kimhyungkyu-1208@users.noreply.github.com>
2022-02-08 22:08:10 +0900

    step3

--- vs_code-workspace/nn_Module_linearRegression.py
+++ vs_code-workspace/nn_Module_linearRegression.py
@@ -1,2 +1,10 @@
-for i in range(1, 101):
-	print('웅' * i, end="!\n")
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+# 재사용을 위해 랜덤값을 초기화 합니다.
+torch.manual_seed(1)
+
+x_train = torch.FloatTensor([[1], [2], [3]])
+y_train = torch.FloatTensor([[3], [6], [9]])
+
